e8302ffade97320007b336124008fe051f80b8f4
QuentinJanuel <facemashldv@laposte.net>
2021-08-26 18:31:50 +0200

    Init commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+export const App = function () {
+	return <div className={ styles.container }>
+		Hello, World!
+	</div>;
+}
